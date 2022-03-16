069acaee0cf7074cd005c5560c50887da7416470
ShinjiSKA <thiago.sakata@fatec.sp.gov.br>
2022-03-16 00:56:33 -0300

    test

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
+    path('notes', views.NotesListView.as_view()),
+    path('notes/<int:pk>', views.NotesDetailView.as_view()),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,23 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = "notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist")
+    return render(request, 'notes/notes_detail.html', {'note': note})
